7bf59b27ab94a61a2ebe370f1f1ce8a8cef14be5
surendrakumar6350 <kamleshksks456@gmail.com>
2025-06-04 04:24:50 -0700

    fix(auth): prevent infinite redirect on /admin/dashboard for admin users

--- ecommerce-mern/client/src/components/common/check-auth.jsx
+++ ecommerce-mern/client/src/components/common/check-auth.jsx
@@ -2,42 +2,44 @@ import { Navigate, useLocation } from "react-router-dom";
 
 
 
-function CheckAuth({isAuthenticated, user, children}) {
+function CheckAuth({ isAuthenticated, user, children }) {
 
     const location = useLocation();
 
-    if(!isAuthenticated &&
+    if (!isAuthenticated &&
         !(
-            location.pathname.includes('/login') || 
+            location.pathname.includes('/login') ||
             location.pathname.includes('/register')
         )
-        )
-        {
-            return <Navigate to={'/auth/login'}/>;
-        }
-    
-        if(isAuthenticated &&
-            (
-                location.pathname.includes('/login') || 
-                location.pathname.includes('/register')
-            )
-            )
-            {
-                if(user?.role === 'admin'){
-                    return <Navigate to={"/admin/dashboard"} />
-                }else{
-                    return <Navigate to={"/shop/home"} />
-                }
-            }
-        if(isAuthenticated && user?.role !== 'admin' && location.pathname.includes('admin')){
-            return <Navigate to={'/unauth-page'}/>
-        }
-        if(isAuthenticated && user?.role === 'admin' && location.pathname.includes('shop')){
-            return <Navigate to={"/admin/dashboard"} />
-        }
+    ) {
+        return <Navigate to={'/auth/login'} />;
+    }
 
-        return <>{children}</>
-    
+    if (isAuthenticated &&
+        (
+            location.pathname.includes('/login') ||
+            location.pathname.includes('/register')
+        )
+    ) {
+        if (user?.role === 'admin') {
+            return <Navigate to={"/admin/dashboard"} />
+        } else {
+            return <Navigate to={"/shop/home"} />
+        }
+    }
+    if (isAuthenticated && user?.role !== 'admin' && location.pathname.includes('admin')) {
+        return <Navigate to={'/unauth-page'} />
+    }
+    if (isAuthenticated && user?.role === 'admin' && location.pathname.includes('shop')) {
+        return <Navigate to={"/admin/dashboard"} />
+    }
+
+    if (isAuthenticated && location.pathname === '/') {
+        return <Navigate to={user?.role === 'admin' ? '/admin/dashboard' : '/shop/home'} />;
+    }
+
+    return <>{children}</>
+
 };
 
 export default CheckAuth;
